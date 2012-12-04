c62ec25914b1c71e7a7435c7b530656be0ba9e25
Esben Nielsen <esben@littlesmartthings.com>
2012-12-04 13:54:00 +0100

    Inial commit

---
+++ WordEditorAPI/src/org/word/editor/api/WordFilter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.word.editor.api;
+
+/**
+ *
+ * @author esben
+ */
+public interface WordFilter {
+    String process(String s);
+}
+
