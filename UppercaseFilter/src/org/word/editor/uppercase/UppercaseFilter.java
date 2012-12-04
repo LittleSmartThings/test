17b77010fe941141bdacf63c1b9417988f373709
Kevin Donovan <git@kevindonovan.net>
2012-12-04 14:48:39 +0100

    initial project commit + connector

--- UppercaseFilter/src/org/word/editor/uppercase/UppercaseFilter.java
+++ UppercaseFilter/src/org/word/editor/uppercase/UppercaseFilter.java
@@ -3,6 +3,8 @@ package org.word.editor.uppercase;
 import org.openide.util.lookup.ServiceProvider;
 import org.word.editor.api.WordFilter;
 
+//ESBEN : FIX det lort!!
+
 @ServiceProvider(service=WordFilter.class)
 public class UppercaseFilter implements WordFilter {
 
